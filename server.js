1b9f1123aa7237f50f1e32f10f0f74588f61190f
thembhanin-lgtm <thembhanin@gmail.com>
2026-07-27 08:00:28 +0000

    Remove welcome route and serve static files
    Removed the welcome route and added static file serving.

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>🛒 Welcome to My Free Store!</h1><p>Tracking system is starting up...</p>');
-});
+// This line tells your server to show the index.html page automatically
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server live on port ${PORT}`));
